06c4f7059bdb9f38fe8af6b27534fed2ed5ba93c
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2019-11-09 13:49:06 +0800

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Tarmizi',
+            'email' => 'admin@mizi.my',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Ali',
+            'email' => 'ali@mizi.my',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
